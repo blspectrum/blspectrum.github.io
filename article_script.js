0b63f907dcb56caba0c56c6073d2affc511799c8
mona <mona@monas-MacBook-Air.local>
2024-10-18 16:22:51 -0700

    update first article

--- article_script.js
+++ article_script.js
@@ -1,29 +1,5 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const article = document.querySelector('article');
-//     const wordCount = article.innerText.split(/\s+/).length;
-//     const readingTime = Math.ceil(wordCount / 200); // Assuming 200 words per minute
-
-//     const timeElement = document.createElement('p');
-//     timeElement.textContent = `Estimated reading time: ${readingTime} minute${readingTime !== 1 ? 's' : ''}`;
-//     article.insertBefore(timeElement, article.firstChild);
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     fetch('realityshow_article/realityshow_article1.txt')
-//         .then(response => response.text())
-//         .then(content => {
-//             document.getElementById('articleContent').innerHTML = content;
-//         })
-//         .catch(error => console.error('Error loading article content:', error));
-
-//     // You can also load other elements dynamically
-//     document.getElementById('articleTitle').textContent = 'Your Dynamic Article Title';
-//     document.getElementById('articleImage').src = 'images/article1-image.jpg';
-//     document.getElementById('articleImage').alt = 'Description of image';
-// });
-
-
 document.addEventListener('DOMContentLoaded', function() {
+    // not used
     const articleId = 'article1'; // You can set this dynamically based on which article is being viewed
     
     fetch(`articles/${articleId}.txt`)
